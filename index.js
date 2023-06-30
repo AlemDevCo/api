5fae711712d08e5aa67403e3742f460007053a93
AlemDevCo <102746202+AlemDevCo@users.noreply.github.com>
2023-06-30 12:15:00 -0400

    Create index.js

---
+++ index.js
@@ -0,0 +1,32 @@
+const express = require("express");
+const axios = require("axios");
+
+const app = express();
+
+const itemIDs = ["1", "2", "3", "4"];
+
+app.get("/", (req, res) => {
+    const { userid, itemid } = req.query;
+    let successful = false;
+
+    const promises = itemIDs.map((id) => {
+        const url = `https://inventory.roblox.com/v1/users/${userid}/items/${id}/${itemid}/is-owned`;
+        return axios.get(url)
+            .then((response) => {
+                if (response.data === true) {
+                    successful = true;
+                }
+            })
+            .catch(() => {});
+    });
+
+    Promise.all(promises).then(() => {
+        if (successful) {
+            res.send("true");
+        } else {
+            res.send("false");
+        }
+    });
+});
+
+app.listen(3000);
